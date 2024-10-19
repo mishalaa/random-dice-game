76f5a444e2e8b6ba9c366acb078b7e7b6bf17ea2
Mishal Chowdhury <100079281+mishalaa@users.noreply.github.com>
2024-10-19 22:26:23 +0600

    Add files via upload

---
+++ dice thinker/index.js
@@ -0,0 +1,10 @@
+// var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
+
+// var diceImage1 = document.getElementsByClassName("img1");
+// var diceImage2 = document.getElementsByClassName("img2");
+
+
+// diceImage1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
+// diceImage2.setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+alert('tmh');
